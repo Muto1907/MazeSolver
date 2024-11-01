4c57d8e47721de01c11d3a2047c6cf0f8b21b235
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-11-01 19:40:44 +0100

    Implemen Line, Point Classes and draw Lines in main. Fix misuse of title Function

--- GUI.py
+++ GUI.py
@@ -3,7 +3,7 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height,):
         self.__root = Tk()
-        self.__root.title = "Mahmut's Maze Solver"
+        self.__root.title("Mahmut's Maze Solver")
         self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__isRunning = False
@@ -20,4 +20,19 @@ class Window:
     
     def close(self):
         self.__isRunning = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, p1, p2):
+        self.__p1 = p1
+        self.__p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line( self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from GUI import Window
+from GUI import (Window, Point, Line )
 def main():
     win = Window(800, 600)
+    p1 = Point(x=0, y=0)
+    p2 = Point(x=255, y=255)
+    line = Line(p1,p2)
+    p3 = Point(256, 255)
+    p4 = Point(512, 0)
+    line2 = Line(p3, p4)
+    win.draw_line(line, "green")
+    win.draw_line(line2, "red")
     win.wait_for_close()
 
 
