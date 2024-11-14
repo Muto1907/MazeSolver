f1b9c614b5d06e02e5638ccf06be3f089db271c2
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-11-14 17:49:21 +0100

    Add draw and animate methods to Maze Class

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from GUI import (Window, Point, Line )
 from cell import (Cell)
+from maze import Maze
 def main():
     win = Window(800, 600)
-    cell = Cell(win)
+    maze = Maze(50,50,5,4,120,50,win)
+    maze._create_cells()
+    win.wait_for_close()
+    """cell = Cell(win)
     cell.draw(110,110,100,100)
     cell2 = Cell(win)
     cell2.draw(500, 500, 900, 900)
     cell.draw_move(cell2)
-    win.wait_for_close()
+ """
+
 
 main()
--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 from cell import Cell
+import time
 class Maze:
     def __init__(
             self,
@@ -25,7 +26,20 @@ class Maze:
             self._cells.append([])
             for j in range (self._num_rows):
                 self._cells[i].append(Cell(self._win))
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        return
+        position_x1 = self._cell_size_x * i + self._x1
+        position_x2 = self._cell_size_x * (i+1) + self._x1
+        position_y1 = self._cell_size_y * j + self._y1
+        position_y2 = self._cell_size_y * (j + 1) + self._y1
+        self._cells[i][j].draw(position_x1, position_y1, position_x2, position_y2)
+        self._animate()
+    
+    def _animate(self):
+        if not self._win:
+            return
+        self._win.redraw
+        time.sleep(0.05)
